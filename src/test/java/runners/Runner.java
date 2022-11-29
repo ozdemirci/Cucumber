63a80bc7f1e0d8642b808f85951a7977262f6eea
ozdemirci <ozdemircihakan@gmail.com>
2022-11-29 14:47:56 +0300

    29/11

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -4,13 +4,19 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-@CucumberOptions (
-        plugin = {"html:target/cucumber-reports.html"},
-        features="src/test/resources/features",
-        glue="stepDefinitions",
-        tags="@US11_TC05",
-        dryRun= false)
-
-
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml",
+                "rerun:target/failedRerun.txt"
+        },
+        features = "./src/test/resources/features",
+        glue = {"stepdefinitions", "hooks"},
+        tags = "@US028",
+        dryRun =false
+)
 public class Runner {
 }
